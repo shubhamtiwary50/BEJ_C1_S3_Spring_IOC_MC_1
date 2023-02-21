ba7e0cd49087ad5b22ac8635cd62d1c688959cee
SHUBHAM <shubhamtiwary50@gmail.com>
2023-02-21 19:05:41 +0530

    Done with the main implementation class.

--- src/main/java/com/niit/App.java
+++ src/main/java/com/niit/App.java
@@ -1,13 +1,17 @@
 package com.niit;
 
+import com.niit.config.BeanConfig;
+import com.niit.domain.Employee;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(BeanConfig.class);
+        Employee employee = applicationContext.getBean(Employee.class);
+        System.out.println(employee);
     }
 }

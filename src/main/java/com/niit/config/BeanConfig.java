352b232af7957a47de55620378214e9ae1be1dc1
SHUBHAM <shubhamtiwary50@gmail.com>
2023-02-21 19:04:43 +0530

    Completed with the Bean config file

--- src/main/java/com/niit/config/BeanConfig.java
+++ src/main/java/com/niit/config/BeanConfig.java
@@ -1,4 +1,18 @@
 package com.niit.config;
 
+import com.niit.domain.Department;
+import com.niit.domain.Employee;
+import org.springframework.context.annotation.Bean;
+
 public class BeanConfig {
+
+    @Bean
+    public Department getDepartment() {
+        return new Department("IOC Department", "47");
+    }
+
+    @Bean
+    public Employee getEmployee() {
+        return new Employee(123, "Employee 234", "123/23 MG Road", getDepartment());
+    }
 }
